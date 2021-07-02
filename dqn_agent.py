b74beca87a70bd4b962cc117f081cb8897be5c4d
Anas Al-Nuaimi <anas.al-nuaimi@bmw.de>
2021-07-02 12:25:44 +0200

    experience replay - doesnt work properly yet

--- dqn_agent.py
+++ dqn_agent.py
@@ -10,20 +10,22 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+BATCH_SIZE = 64  # minibatch size
+GAMMA = 0.99  # discount factor
+TAU = 1e-3  # for soft update of target parameters
+LR = 5e-4  # learning rate
+UPDATE_EVERY = 4  # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"found device: {device}")
 
+
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed, drop_p=0.0, hidden_layers_config=[64,64],
-                 duelling_networks=False):
+    def __init__(self, state_size, action_size, seed, drop_p=0.0, hidden_layers_config=[64, 64],
+                 duelling_networks=False,
+                 priority_experience=False):
         """Initialize an Agent object.
 
         Params
@@ -37,12 +39,14 @@ class Agent():
         self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed, drop_p=drop_p,hidden_layers_config=hidden_layers_config).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed, drop_p=drop_p,hidden_layers_config=hidden_layers_config).to(device)
+        self.qnetwork_local = QNetwork(state_size, action_size, seed, drop_p=drop_p,
+                                       hidden_layers_config=hidden_layers_config).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed, drop_p=drop_p,
+                                        hidden_layers_config=hidden_layers_config).to(device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed, priority_experience=priority_experience)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
 
@@ -56,8 +60,12 @@ class Agent():
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
-        self.memory.add(state, action, reward, next_state, done)
-        
+        # Adapted by Anas for experience replay
+        max_term = self.qnetwork_target(torch.tensor([next_state])).detach().max(dim=1, keepdim=True)[0]
+        abs_td_error = abs(reward + GAMMA * max_term - \
+                           self.qnetwork_local(torch.tensor([state])).gather(dim=1, index=torch.tensor([[action]])))
+        self.memory.add(state, action, reward, next_state, done, abs_td_error)
+
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
@@ -97,12 +105,12 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
         ## TODO: compute and minimize the loss
         "*** YOUR CODE HERE ***"
-        
-        #ANAS CODE
-        self.qnetwork_local.train()       
+
+        # ANAS CODE
+        self.qnetwork_local.train()
         self.optimizer.zero_grad()
         output = self.qnetwork_local(states)
-        output_for_chosen_actions = output.gather(dim=1,index=actions)
+        output_for_chosen_actions = output.gather(dim=1, index=actions)
 
         # You can replace max with mean just like SARSAmean
         expected_output = rewards + gamma * self.compute_max_term(next_states) * (1 - dones)
@@ -111,8 +119,8 @@ class Agent():
         loss.backward()
         self.optimizer.step()
 
-        #self.running_loss.append(loss.norm().item())
-        #print(f"\rlatest running loss: {self.running_loss[-1]}", end="")
+        # self.running_loss.append(loss.norm().item())
+        # print(f"\rlatest running loss: {self.running_loss[-1]}", end="")
 
         """
         if steps % print_every == 0:
@@ -133,14 +141,14 @@ class Agent():
             # Make sure training is back on
             model.train()
         """
-        
+
         # ------------------- update target network ------------------- #
         self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
 
     def compute_max_term(self, next_states):
         if self.duelling_networks:
             actions_maximizing_q = self.qnetwork_local(next_states).max(dim=1, keepdim=True)[1]
-            return self.qnetwork_target(next_states).gather(dim=1,index=actions_maximizing_q)
+            return self.qnetwork_target(next_states).gather(dim=1, index=actions_maximizing_q)
         else:
             return self.qnetwork_target(next_states).max(dim=1, keepdim=True)[0]
 
@@ -155,13 +163,13 @@ class Agent():
             tau (float): interpolation parameter 
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
 
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    def __init__(self, action_size, buffer_size, batch_size, seed, priority_experience=False):
         """Initialize a ReplayBuffer object.
 
         Params
@@ -172,27 +180,44 @@ class ReplayBuffer:
             seed (int): random seed
         """
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  
+        self.memory = deque(maxlen=buffer_size)
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
-    
-    def add(self, state, action, reward, next_state, done):
+
+        # ANAS
+        self.priority_experience = priority_experience
+        self.a = 0.8
+
+    def add(self, state, action, reward, next_state, done, probability=None):
         """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-    
+        self.memory.append((e, probability))
+
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
+        if self.priority_experience:
+            experiences = self.get_experiences_with_priority()
+        else:
+            experiences = self.get_experiences()
 
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-  
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
+            device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
+            device)
+
         return (states, actions, rewards, next_states, dones)
+
+    def get_experiences(self):
+        return [memory[0] for memory in random.sample(self.memory, k=self.batch_size)]
+
+    def get_experiences_with_priority(self):
+        weights = [memory[1] ** self.a for memory in self.memory]
+        assert None not in weights
+        return [memory[0] for memory in random.choices(self.memory, k=self.batch_size, weights=weights)]
 
     def __len__(self):
         """Return the current size of internal memory."""
