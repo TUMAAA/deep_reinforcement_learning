e30773387c0676ee2db41bcac0e4bec35b34bb83
Anas Al-Nuaimi <anas.al-nuaimi@bmw.de>
2021-07-02 12:39:53 +0200

    outdated comments

--- model.py
+++ model.py
@@ -22,8 +22,7 @@ class QNetwork(nn.Module):
         # ANAS
 
         print(f"state_size: {state_size}, action_size: {action_size}")
-        # hidden_layers = [64, 64]  # ANAS MAKE constructor arg
-        # drop_p = 0.0  # ANAS make constructor arg
+
         self.hidden_layers_config=hidden_layers_config
         self.hidden_layers = nn.ModuleList([nn.Linear(state_size, hidden_layers_config[0])])
 
@@ -51,7 +50,6 @@ class QNetwork(nn.Module):
         """Build a network that maps state -> action values."""
 
         # ANAS
-        # Forward through each layer in `hidden_layers`, with ReLU activation and dropout
         x = state
 
         for linear in self.hidden_layers:
